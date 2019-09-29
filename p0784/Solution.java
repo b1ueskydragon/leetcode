c488dc496ff394c5d63d50e668edb9591b6e7fe6
ina <dragoalie@gmail.com>
2019-09-30 01:00:03 +0900

    Simplify with replace to lambda

--- p0784/Solution.java
+++ p0784/Solution.java
@@ -1,19 +1,14 @@
 package leetcode.p0784;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
   private static <T> List<List<T>> product2(List<T> xs, List<T> ys) {
-    List<List<T>> product = new ArrayList<>();
-    for (T x : xs) {
-      for (T y : ys) {
-        List<T> chunk = new ArrayList<>() {{ add(x); }};
-        chunk.add(y);
-        product.add(chunk);
-      }
-    }
-    return product;
+    return new ArrayList<>() {{
+      xs.forEach(x -> ys.forEach(y -> add(Arrays.asList(x, y))));
+    }};
   }
 
 //  private <T> List<List<T>> product(List<List<T>> lists) {
@@ -26,10 +21,6 @@ public class Solution {
 
   public static void main(String[] args) {
     product2(List.of('a', 'A'), List.of('b', 'B'))
-        .forEach(list -> {
-          System.out.print("(");
-          list.forEach(t -> System.out.printf(" %s ", t));
-          System.out.print(")");
-        });
+        .forEach(list -> System.out.printf("%s ", list));
   }
 }
