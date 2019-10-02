1d36c60aff4cb77515382c5db0293b239e46e328
ina <dragoalie@gmail.com>
2019-10-02 13:28:08 +0900

    TODO folding or merge

--- p0784/Solution.java
+++ p0784/Solution.java
@@ -1,25 +1,41 @@
 package leetcode.p0784;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * coding style jdk 8
+ */
 public class Solution {
+
   private static <T> List<List<T>> product2(List<T> xs, List<T> ys) {
     return xs.stream().flatMap(x -> ys.stream().map(y -> Arrays.asList(x, y)))
         .collect(Collectors.toList());
   }
 
-//  private <T> List<List<T>> product(List<List<T>> lists) {
-//
+//  private static <T> List<List<T>> product(List<List<T>> lists) {
+//    if (lists.isEmpty()) return Collections.emptyList();
+//    else if (lists.size() == 1) return Collections.unmodifiableList(lists);
+//    else if (lists.size() == 2) return product2(lists.get(0), lists.get(1));
+//    else
 //  }
-//
+
 //  public List<String> letterCasePermutation(String S) {
 //
 //  }
 
+  /**
+   * coding style jdk 11 for test
+   */
   public static void main(String[] args) {
     product2(List.of('a', 'A'), List.of('b', 'B'))
         .forEach(list -> System.out.printf("%s ", list));
+
+//    System.out.print(System.getProperty("line.separator"));
+//
+//    product(List.of(List.of('a', 'A'), List.of('b', 'B')))
+//        .forEach(list -> System.out.printf("%s ", list));
   }
 }
