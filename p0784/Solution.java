29968c61cff402809d70ed5ce9b52a4545c03422
ina <dragoalie@gmail.com>
2019-10-03 11:51:28 +0900

    Testing flatmap returns stream, Fix testcases, Add TODOs

--- p0784/Solution.java
+++ p0784/Solution.java
@@ -15,12 +15,13 @@ public class Solution {
         .collect(Collectors.toList());
   }
 
-//  private static <T> List<List<T>> product(List<List<T>> lists) {
-//    if (lists.isEmpty()) return Collections.emptyList();
-//    else if (lists.size() == 1) return Collections.unmodifiableList(lists);
-//    else if (lists.size() == 2) return product2(lists.get(0), lists.get(1));
-//    else
-//  }
+  private static <T> List<List<T>> product(List<List<T>> lists) {
+    if (lists.isEmpty()) return Collections.emptyList();
+    else if (lists.size() == 1) return Collections.unmodifiableList(lists);
+    else if (lists.size() == 2) return product2(lists.get(0), lists.get(1));
+    else return lists.subList(1, lists.size()).stream().flatMap(ys -> product2(lists.get(0), ys).stream())
+          .collect(Collectors.toList());  // TODO make a fold function
+  }
 
 //  public List<String> letterCasePermutation(String S) {
 //
@@ -30,12 +31,27 @@ public class Solution {
    * coding style jdk 11 for test
    */
   public static void main(String[] args) {
-    product2(List.of('a', 'A'), List.of('b', 'B'))
-        .forEach(list -> System.out.printf("%s ", list));
+    product2(
+        List.of('a', 'A'),
+        List.of('b', 'B')
+    ).forEach(list -> System.out.printf("%s ", list));
 
-//    System.out.print(System.getProperty("line.separator"));
-//
-//    product(List.of(List.of('a', 'A'), List.of('b', 'B')))
-//        .forEach(list -> System.out.printf("%s ", list));
+    System.out.print(System.getProperty("line.separator"));
+
+    // a
+    product2(
+        List.of(List.of('a', 'b'), List.of('a', 'B'), List.of('A', 'b'), List.of('A', 'B')),
+        List.of('c', 'C')
+    ).forEach(list -> System.out.printf("%s ", list));
+
+    System.out.print(System.getProperty("line.separator"));
+
+    // b
+    product(
+        List.of(List.of('a', 'A'), List.of('b', 'B'), List.of('c', 'C'))
+    ).forEach(list -> System.out.printf("%s ", list));
+
+    // TODO a and b should be same
+
   }
 }
