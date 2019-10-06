390311bd74da9d32f9b22b20aca3737712c5b78b
ina <dragoalie@gmail.com>
2019-10-06 13:09:20 +0900

    solve with backtracking

--- p0784/Solution.java
+++ p0784/Solution.java
@@ -1,20 +1,32 @@
 package leetcode.p0784;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
+/** backtracking */
 public class Solution {
+  private static void permutation(String S, char[] chunk, int pos, List<String> res) {
+    if (pos == S.length()) { // pos to on the end
+      res.add(String.valueOf(chunk));
+      return;
+    }
+    char curr = S.charAt(pos);
+    chunk[pos] = curr;
+    permutation(S, chunk, pos + 1, res);
 
-//  private static <T> List<List<T>> product(List<List<T>> lists) {
-//    if (lists.isEmpty()) return Collections.emptyList();
-//    else if (lists.size() == 1) return Collections.unmodifiableList(lists);
-//    else // TODO
-//  }
+    if (curr >= 97 && curr <= 122) { // lower cases
+      chunk[pos] = (char) (curr - 32); // replace in-place to upper
+      permutation(S, chunk, pos + 1, res);
+    }
+    if (curr >= 65 && curr <= 90) { // upper cases
+      chunk[pos] = (char) (curr + 32); // replace in-place to lower
+      permutation(S, chunk, pos + 1, res);
+    }
+  }
 
-//  public List<String> letterCasePermutation(String S) {
-//  }
-
-  public static void main(String[] args) {
-
+  public List<String> letterCasePermutation(String S) {
+    List<String> res = new ArrayList<>();
+    permutation(S, new char[S.length()], 0, res);
+    return res;
   }
 }
