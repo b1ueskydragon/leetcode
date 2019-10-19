00c96b3cf00c32d9b07012060983b022bad1b5a1
ina <dragoalie@gmail.com>
2019-10-20 03:11:40 +0900

    left-first dfs with go deeper with while-loop

--- p0114/Solution.java
+++ p0114/Solution.java
@@ -28,3 +28,19 @@ class Solution {
     prevRoot = root;
   }
 }
+
+class Solution01 {
+
+  public void flatten(TreeNode root) {
+    if (root == null) return;
+    flatten(root.left);
+    flatten(root.right);
+    TreeNode tmp = root.right;
+    root.right = root.left;
+    root.left = null;
+    while (root.right != null) {
+      root = root.right;
+    }
+    root.right = tmp;
+  }
+}
