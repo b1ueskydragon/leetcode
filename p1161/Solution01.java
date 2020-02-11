343b7f2f2ded82b364dc7d3dabbae7529ecccdef
ina <dragoalie@gmail.com>
2020-02-11 13:57:30 +0900

    solved - generate a level-sum array at first

--- p1161/Solution01.java
+++ p1161/Solution01.java
@@ -1,17 +1,28 @@
 package leetcode.p1161;
 
 class Solution01 {
+
+  // make as global to stable status
+  private static int maxLevel = 1;
   // The number of nodes in the given tree is between 1 and 10^4
   private static int[] nodes = new int[10000];
-  private static int maxLevel = 1;
 
   public int maxLevelSum(TreeNode root) {
-    return -1; // TODO
+    dfs(root, 1);
+    int maxRes = root.val;
+    int resLevel = 1;
+    for (int i = 1; i < maxLevel; i++) {
+      if (nodes[i] > maxRes) {
+        maxRes = nodes[i];
+        resLevel = i;
+      }
+    }
+    return resLevel;
   }
 
   private static void dfs(TreeNode root, int currLevel) {
     if (root == null) return; // base
-    maxLevel = (currLevel > maxLevel) ? currLevel : maxLevel;
+    maxLevel = (currLevel > maxLevel) ? currLevel : maxLevel; // global
     nodes[currLevel] += root.val;
     dfs(root.left, currLevel + 1);
     dfs(root.right, currLevel + 1);
