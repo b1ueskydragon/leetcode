1427e8f2cf5cd595e32acad3e4fe1ff59baa0510
ina <dragoalie@gmail.com>
2020-04-03 11:18:26 +0900

    LCP - binary search

---
+++ p0004/Solution.java
@@ -0,0 +1,27 @@
+package leetcode.p0004;
+
+class Solution {
+  private static boolean isCommonPrefix(String[] strs, String prefix) {
+    for (String str : strs) {
+      if (!str.startsWith(prefix)) return false;
+    }
+    return true;
+  }
+
+  public String longestCommonPrefix(String[] strs) {
+    if (strs == null || strs.length == 0) return "";
+
+    final String maybeLcp = strs[0];
+    int low = 1;
+    int high = maybeLcp.length();
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (isCommonPrefix(strs, maybeLcp.substring(0, mid))) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return maybeLcp.substring(0, (low + high) / 2);
+  }
+}
