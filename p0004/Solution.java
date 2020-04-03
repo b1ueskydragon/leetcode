8f9be4a8e1f277cb14dc1841166dd3704e465766
ina <dragoalie@gmail.com>
2020-04-03 11:30:27 +0900

    Set the limit of right-end with minimal length is also OK

--- p0004/Solution.java
+++ p0004/Solution.java
@@ -10,10 +10,13 @@ class Solution {
 
   public String longestCommonPrefix(String[] strs) {
     if (strs == null || strs.length == 0) return "";
-
+    int minLen = Integer.MAX_VALUE;
+    for (String str : strs) {
+      minLen = Math.min(minLen, str.length());
+    }
     final String maybeLcp = strs[0];
     int low = 1;
-    int high = maybeLcp.length();
+    int high = minLen;
     while (low <= high) {
       int mid = (low + high) / 2;
       if (isCommonPrefix(strs, maybeLcp.substring(0, mid))) {
