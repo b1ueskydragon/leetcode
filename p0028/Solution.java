5f08f4d2f9f5ff21059aefc8c557e8e175116dfa
ina <dragoalie@gmail.com>
2020-04-04 13:19:05 +0900

    memo memo

--- p0028/Solution.java
+++ p0028/Solution.java
@@ -2,6 +2,28 @@ package leetcode.p0028;
 
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        int fromIndex = 0;
+        // haystack = len N
+        // needle = len K
+
+        // find a start point of haystack where starts with needle's head char
+        // retrieve k, k <= O(K)
+        // if not found, skip k + head
+
+        // bbb bbbbbbbbab
+        // bab
+        // + K
+        // bbb bbb bbbbbab
+        //     bab
+        // + K
+        // bbbbbb bbb bbab
+        //        bab
+        // + K
+        // bbbbbbbbb bba b
+        //           bab
+        // + 1
+        // bbbbbbbbbb bab
+        //            bab
+        return -1;
     }
 }
