9c74c2f3bdb385d22aabf56391664369fc42b1c7
ina <dragoalie@gmail.com>
2020-04-05 12:34:45 +0900

    Introduce KMP to reuse the pattern in subStr

--- p0028/Solution.java
+++ p0028/Solution.java
@@ -1,29 +1,43 @@
 package leetcode.p0028;
 
+import java.util.Arrays;
+
 class Solution {
-    public int strStr(String haystack, String needle) {
-        int fromIndex = 0;
-        // haystack = len N
-        // needle = len K
 
-        // find a start point of haystack where starts with needle's head char
-        // retrieve k, k <= O(K)
-        // if not found, skip k + head
-
-        // bbb bbbbbbbbab
+    private static int[] skipTable(String subStr) {
+        final int K = subStr.length();
+        // ba_________
         // bab
-        // + K
-        // bbb bbb bbbbbab
-        //     bab
-        // + K
-        // bbbbbb bbb bbab
-        //        bab
-        // + K
-        // bbbbbbbbb bba b
-        //           bab
-        // + 1
-        // bbbbbbbbbb bab
-        //            bab
+        // [matched count:skippable count]
+        // [0:1, 1:1, 2:2]
+        // ->     b  ba
+        //    [1, 1, 2]
+        final int[] table = new int[K];
+        table[0] = 1; // the case of different from the first char.
+        int i = 0, j = 0;
+        while (i < K && j < K) {
+            // [b] a b
+            // [b] a b
+            if (subStr.charAt(i) == subStr.charAt(j)) {
+                table[j] = i + 1;
+                i++;
+                //  b [a] b
+                // [b] a  b
+            } else {
+                //   b [a] b
+                //  [b] a  b
+                table[j] = 1; // normally skip (+1);
+                j++;
+            }
+        }
+        return table;
+    }
+
+    public int strStr(String haystack, String needle) {
+        Arrays.stream(skipTable(needle)).forEach(System.out::println);
+//        int i = 0;
+//        while (i < haystack.length()) {
+//        }
         return -1;
     }
 }
