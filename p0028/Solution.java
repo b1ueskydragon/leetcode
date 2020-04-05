1e9b7464863d2ec13d881f66bad83ea7f86745f8
ina <dragoalie@gmail.com>
2020-04-05 21:30:16 +0900

    KMP - using skip table to skip inside of subStr

--- p0028/Solution.java
+++ p0028/Solution.java
@@ -1,43 +1,34 @@
 package leetcode.p0028;
 
-import java.util.Arrays;
-
 class Solution {
-
-    private static int[] skipTable(String subStr) {
-        final int K = subStr.length();
-        // ba_________
-        // bab
-        // [matched count:skippable count]
-        // [0:1, 1:1, 2:2]
-        // ->     b  ba
-        //    [1, 1, 2]
-        final int[] table = new int[K];
-        table[0] = 1; // the case of different from the first char.
-        int i = 0, j = 0;
-        while (i < K && j < K) {
-            // [b] a b
-            // [b] a b
-            if (subStr.charAt(i) == subStr.charAt(j)) {
-                table[j] = i + 1;
-                i++;
-                //  b [a] b
-                // [b] a  b
-            } else {
-                //   b [a] b
-                //  [b] a  b
-                table[j] = 1; // normally skip (+1);
-                j++;
-            }
-        }
-        return table;
+  // This doesn't depend on originalStr.
+  public static int[] skipTable(String subStr) {
+    final int K = subStr.length();
+    final int[] table = new int[K + 1]; // table[0] is a sentinel.
+    int i = 0, j = 1;
+    while (j < K) {
+      if (subStr.charAt(i) == subStr.charAt(j)) {
+        table[++j] = ++i; // increment first, since of the sentinel index.
+      } else {
+        table[++j] = table[i];
+      }
     }
+    return table;
+  }
 
-    public int strStr(String haystack, String needle) {
-        Arrays.stream(skipTable(needle)).forEach(System.out::println);
-//        int i = 0;
-//        while (i < haystack.length()) {
-//        }
-        return -1;
+  public int strStr(String haystack, String needle) {
+    final int[] skipTable = skipTable(needle);
+    int n = 0, k = 0;
+    while (k < needle.length() && n < haystack.length()) {
+      if (haystack.charAt(n) == needle.charAt(k)) {
+        n++;
+        k++;
+      } else if (k == 0) {
+        n++;
+      } else {
+        k = skipTable[k]; // Don't move n yet.
+      }
     }
+    return k == needle.length() ? n - k : -1;
+  }
 }
