90c9124788f9f3198d605aefbe135e0a0c9543fa
ina <dragoalie@gmail.com>
2020-04-06 22:57:10 +0900

    solution - slower than KMP but simple-smart"

--- p0028/Solution01.java
+++ p0028/Solution01.java
@@ -5,16 +5,19 @@ package leetcode.p0028;
  * it should be,
  *
  *          [           ]         -- subStr
- *         j1.......    jk
+ *          1.......   j
  *         /          /
- * [      [          ]          ] -- Str
- *     j1 + i.....  jk + i
+ * [      [          ]          ] -- originalStr
+ *       1+i......  j+i
+ *
+ *  O(k * N), N is the length of originalStr.
  */
 class Solution01 {
   public int strStr(String haystack, String needle) {
-    for (int i = 0; ; i++) { // i is an index of an Str.
-      for (int j = i; ; j++) { // j is an index of a subStr.
-
+    for (int i = 0; ; i++) { // i is an index of an originalStr.
+      for (int j = 0; ; j++) { // j is an index of a subStr. we should retrieve subStr from 0 for each i.
+        if (j == needle.length()) return i; // we found. just return a buffer index.
+        if (i + j == haystack.length()) return -1; // overflowed from originalStr
         if (needle.charAt(j) != haystack.charAt(j + i)) break; // go to the next i
       }
     }
