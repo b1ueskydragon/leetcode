0f4924db195d80b0b19e37d283b71cb1719ce76a
ina <dragoalie@gmail.com>
2020-04-15 22:34:39 +0900

    solved - reverse a singly linked list

---
+++ p0206/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.p0206;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode(int x) { val = x; }
+}
+
+class Solution {
+  // head, res, prev, tmp .. they're just pointers.
+  // But they are sharing the same object and they can change the object at the same way.
+  public ListNode reverseList(ListNode head) {
+    ListNode res = head;
+    ListNode prev = null;
+    while (head != null) {
+      ListNode tmp = head.next; // copy the `pointer` first. Separate the `next`.
+      res.next = prev;
+      prev = res; // move to the prev.
+      head = tmp;
+      res = head;
+    }
+    return prev;
+  }
+}
