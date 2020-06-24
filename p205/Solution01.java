0a7092a197fa3514b10d0f4343a71599058b3718
ina <dragoalie@gmail.com>
2020-06-24 15:12:17 +0900

    reduce space - one-to-one mapping; using small array. constrain targeting only lowercase

---
+++ p205/Solution01.java
@@ -0,0 +1,25 @@
+package leetcode.p205;
+
+class Solution01 {
+  public boolean isIsomorphic(String s, String t) {
+    final int n = s.length();
+    // only targeting a lowercase
+    final char[] xRange = new char[26];
+    final char[] yRange = new char[26];
+
+    final char[] S = s.toCharArray();
+    final char[] T = t.toCharArray();
+
+    for (int i = 0; i < n; i++) {
+      char x = S[i], y = T[i];
+      if (xRange[x % 97] == 0 && yRange[y % 97] == 0) {
+        xRange[x % 97] = y;
+        yRange[y % 97] = x;
+      } else if (xRange[x % 97] != y || yRange[y % 97] != x) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
