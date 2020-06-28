36a2fc592a93f8da33fdd68b2dfeda5ea7847f53
ina <dragoalie@gmail.com>
2020-06-28 13:23:59 +0900

    solved - change the destination dynamically; O(N)

---
+++ p0055/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.p0055;
+
+public class Solution {
+  public boolean canJump(int[] nums) {
+    int goal = nums.length - 1; // dynamic goal.
+    // think from the goal (count backwards).
+    for (int i = goal - 1; i >= 0; i--) {
+      // We should retrieve each point at least and at most once O(N),
+      // since there is chance that the point bigger than goal exists
+      if (i + nums[i] >= goal) {
+        goal = i;
+      }
+    }
+    // reached to start point or not after finish counting backwards.
+    return goal == 0;
+  }
+}
