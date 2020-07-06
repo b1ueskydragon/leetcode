52663a163bce4c7a8f6cdf9b7a82139c7fcfa8b0
ina <dragoalie@gmail.com>
2020-07-06 20:45:16 +0900

    Simplify, anyway

--- p0322/Solution.java
+++ p0322/Solution.java
@@ -3,24 +3,17 @@ package leetcode.p0322;
 class Solution {
   public int coinChange(int[] coins, int amount) {
     int minCount = Integer.MAX_VALUE;
-    int[] memo = new int[amount + 1];
-    minCount = dfs(memo, minCount, coins, amount);
-    // System.out.print("[");
-    // for (int x: memo) {
-    //     System.out.printf("%d , ", x);
-    // }
-    // System.out.print("]\n");
+    minCount = dfs(minCount, coins, amount);
     return minCount < 0 ? -1 : minCount;
   }
 
-  private int dfs(int[] memo, int minCount, int[] coins, int amount) {
+  private int dfs(int minCount, int[] coins, int amount) {
     if (amount == 0) { // base case.
       return 0;
     }
     for (int coin : coins) {
       if (amount < coin) continue;
-      minCount = Math.min(dfs(memo, minCount, coins, amount - coin) + 1, minCount);
-      memo[amount] = minCount;
+      minCount = Math.min(dfs(minCount, coins, amount - coin) + 1, minCount);
     }
     return minCount;
   }
