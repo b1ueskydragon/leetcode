2c9370d913b68340856d9aacd4efdef943e626b5
ina <dragoalie@gmail.com>
2020-09-07 14:03:41 +0900

    introduce how to remove the least significant bit 1

---
+++ p0191/improve/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.p0191.improve;
+
+// Remove the least significant 1 in each loop.
+public class Solution {
+  // you need to treat n as an unsigned value
+  public int hammingWeight(int n) {
+    int cnt = 0;
+    while (n != 0) {
+      cnt++;
+      n = n & (n - 1);
+    }
+    return cnt;
+  }
+}
