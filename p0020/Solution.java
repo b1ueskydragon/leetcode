6b9e020fe87a32019d4411c0b373d970ab15e62f
ina <dragoalie@gmail.com>
2020-09-14 23:21:48 +0900

    solved - simplify with Early return. is valid parentheses with Stack

--- p0020/Solution.java
+++ p0020/Solution.java
@@ -5,33 +5,28 @@ import java.util.Map;
 import java.util.Stack;
 
 class Solution {
-  final Map<Character, Character> pair = new HashMap<>() {{
-    put('(', ')');
-    put('{', '}');
-    put('[', ']');
-  }};
-  final Stack<Character> matchResult = new Stack<>();
-
   public boolean isValid(String s) {
+    final Map<Character, Character> table = new HashMap<>();
+    table.put('(', ')');
+    table.put('[', ']');
+    table.put('{', '}');
+
+    final Stack<Character> stack = new Stack<>();
     for (char c : s.toCharArray()) {
-      if (matchResult.isEmpty() && !pair.containsKey(c)) {
-        // cannot build a pair.
-        return false;
-      }
-      if (pair.containsKey(c)) {
-        // key '(', '{', '[' part. just push.
-        matchResult.push(c);
+      if (table.containsKey(c)) {
+        stack.push(c);
         continue;
       }
-      // value ')', '}', ']' part. pop only if paired.
-      if (!matchResult.isEmpty() &&
-          c == pair.get(matchResult.peek())) {
-        matchResult.pop();
-      } else if (c != pair.get(matchResult.peek())) {
-        // cannot build a pair.
+
+      // since we don't push ')' ']' '}' in the stack.
+      if (stack.isEmpty()) {
+        return false;
+      }
+
+      if (c != table.get(stack.pop())) {
         return false;
       }
     }
-    return matchResult.isEmpty();
+    return stack.isEmpty();
   }
 }
