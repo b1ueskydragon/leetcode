51200b525d7f8d98c2d59a225f35da5bb4eed344
ina <dragoalie@gmail.com>
2020-09-14 11:08:57 +0900

    solved - compute overlapped width and height

---
+++ p0836/Solution01.java
@@ -0,0 +1,24 @@
+package leetcode.p0836;
+
+public class Solution01 {
+  public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+    // bottom-left corner
+    final int a = rec1[0];
+    final int b = rec1[1];
+    // top-right corner
+    final int c = rec1[2];
+    final int d = rec1[3];
+
+    // bottom-left corner
+    final int e = rec2[0];
+    final int f = rec2[1];
+    // top-right corner
+    final int g = rec2[2];
+    final int h = rec2[3];
+
+    boolean isWidthOverlapped = (g - a) > 0 && (c - e) > 0;
+    boolean isHeightOverlapped = (h - b) > 0 && (d - f) > 0;
+
+    return isWidthOverlapped && isHeightOverlapped;
+  }
+}
