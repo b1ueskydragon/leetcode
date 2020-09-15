b8911c91b6cb805739187e817dd05275c3163c66
ina <dragoalie@gmail.com>
2020-09-15 09:41:50 +0900

    solved - from the least significant to the most. restrict to call the loop just 31 times

---
+++ p0190/Solution.java
@@ -0,0 +1,19 @@
+package leetcode.p0190;
+
+public class Solution {
+  // you need treat n as an unsigned value
+  // n is 32 bits unsigned integer
+  // ... not (1 << 32) - 1 - n
+  // reverse from left to right
+  public int reverseBits(int n) {
+    int rn = 0;
+    // set a limit of the loop calls
+    for (int i = 0; i < 31; i++) {
+      rn += (n - ((n >> 1) << 1));
+      rn <<= 1;
+      n >>= 1;
+    }
+    // add bit 1 if n remains
+    return rn + Math.abs(n);
+  }
+}
