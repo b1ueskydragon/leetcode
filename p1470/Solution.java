a7c149f7ff3169ca684e2da07df3e3745e2d1539
ina <dragoalie@gmail.com>
2020-10-26 19:37:12 +0900

    solved - shuffle the array with multi cursors. O(N)

---
+++ p1470/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.p1470;
+
+class Solution {
+  // nums.length == 2n
+  public int[] shuffle(int[] nums, int n) {
+    final int[] shuffled = new int[nums.length];
+    int i = 0;
+    int xi = 0;
+    int yi = n;
+    while (i < nums.length) {
+      shuffled[i] = (i % 2 == 0) ? nums[xi++] : nums[yi++];
+      i++;
+    }
+    return shuffled;
+  }
+}
