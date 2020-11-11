68de94aa61e761c2a136c8aba8784e34949967a1
ina <dragoalie@gmail.com>
2020-11-11 22:25:30 +0900

    solved - find judge by checking hash tables contains certain key or not

---
+++ p0997/Solutioni.java
@@ -0,0 +1,50 @@
+package leetcode.p0997;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+  public int findJudge(int N, int[][] trust) {
+    if (trust.length == 0 && N > 1) {
+      return -1;
+    }
+
+    final Map<Integer, Set<Integer>> personToTrust = new HashMap<>();
+    for (int[] pair : trust) {
+      final int person = pair[0];
+      final int maybeJudge = pair[1];
+
+      if (personToTrust.containsKey(person)) {
+        personToTrust.get(person).add(maybeJudge);
+        continue;
+      }
+      final Set<Integer> candidates = new HashSet<>();
+      candidates.add(maybeJudge);
+      personToTrust.put(person, candidates);
+    }
+
+
+    if (personToTrust.keySet().size() < N - 1) {
+      return -1;
+    }
+
+    int judge = -1;
+
+    for (int i = 1; i <= N; i++) {
+      if (!personToTrust.containsKey(i)) {
+        judge = i;
+        break;
+      }
+    }
+
+    for (int person : personToTrust.keySet()) {
+      if (!personToTrust.get(person).contains(judge)) {
+        return -1;
+      }
+    }
+
+    return judge;
+  }
+}
