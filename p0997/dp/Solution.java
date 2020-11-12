207dfdec34000954875b5cc22a7bbc809b47330b
ina <dragoalie@gmail.com>
2020-11-12 18:07:30 +0900

    improve space and spped - find judge. using array to count vote

---
+++ p0997/dp/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.p0997.dp;
+
+class Solution {
+  public int findJudge(int N, int[][] trust) {
+    final int[] votes = new int[N + 1]; // votes per each person. index 0 is a sentinel.
+
+    for (int[] pair : trust) {
+      votes[pair[0]] = -1; // cannot be a candidate.
+      votes[pair[1]]++; // maybe candidate.
+    }
+
+    for (int i = 1; i <= N; i++) {
+      if (votes[i] == N - 1) return i; // judge should get N-1 votes (judge cannot vote itself).
+    }
+
+    return -1;
+  }
+}
