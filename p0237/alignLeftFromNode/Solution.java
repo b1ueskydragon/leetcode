ab9d8a907f0be67e8b1ce2b0ea4d396b73979582
ina <dragoalie@gmail.com>
2020-12-09 12:57:44 +0900

    separate to packages, two possible solutions

--- p0237/alignLeftFromNode/Solution.java
+++ p0237/alignLeftFromNode/Solution.java
@@ -1,4 +1,4 @@
-package leetcode.p0237;
+package leetcode.p0237.alignLeftFromNode;
 
 
 class ListNode {
---
+++ p0237/alignOnceAndSkip/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.p0237.alignOnceAndSkip;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode(int x) { val = x; }
+}
+
+/**
+ * You will not be given access to the head of the list,
+ * instead you will be given access to the node to be deleted directly.
+ * It is guaranteed that the node to be deleted is not a tail node in the list.
+ * The value of each node in the list is unique.
+ */
+class Solution {
+  public void deleteNode(ListNode node) {
+    // align value to left
+    node.val = node.next.val;
+    node.next = node.next.next;
+  }
+}
