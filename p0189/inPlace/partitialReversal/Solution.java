47f9acc3da497443b5038d18470393813ca74c21
ina <dragoalie@gmail.com>
2021-01-10 00:23:49 +0900

    solved - In-place, to repeat reversing partially. rotate the array to the right by k steps

---
+++ p0189/inPlace/partitialReversal/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.p0189.inPlace.partitialReversal;
+
+class Solution {
+  // O(k*n) time, O(1) space
+  public void rotate(int[] nums, int k) {
+    final int n = nums.length;
+    k %= n;
+    for (int i = 0; i < k; i++) {
+      // start point
+      int kth = n - k + i;
+      swap(nums, i, kth);
+
+      // tails
+      int j = i + 1;
+      int time = 0;
+      while (time < n - k) {
+        swap(nums, j, kth);
+        j++;
+        time++;
+      }
+    }
+  }
+
+  private static void swap(int[] nums,
+                           int i,
+                           int j) {
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  }
+}
