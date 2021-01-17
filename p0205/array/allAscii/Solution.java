3e46ab85c70134d01efe40f6f1c865a07d73cd9b
ina <dragoalie@gmail.com>
2021-01-17 10:08:43 +0900

    solved - one-to-one character mapping. 256 length array

---
+++ p0205/array/allAscii/Solution.java
@@ -0,0 +1,28 @@
+package leetcode.p0205.array.allAscii;
+
+// Isomorphic Strings
+class Solution {
+  // t.length == s.length.
+  // s and t consist of any valid ascii character.
+  public boolean isIsomorphic(String s, String t) {
+    final int[] xRange = new int[256];
+    final int[] yRange = new int[256];
+    final char[] xs = s.toCharArray();
+    final char[] ys = t.toCharArray();
+
+    for (int i = 0; i < xs.length; i++) {
+      int x = xs[i];
+      int y = ys[i];
+      if (xRange[x] == 0) {
+        xRange[x] = y;
+      }
+      if (yRange[y] == 0) {
+        yRange[y] = x;
+      }
+      if (xRange[x] != y || yRange[y] != x) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
