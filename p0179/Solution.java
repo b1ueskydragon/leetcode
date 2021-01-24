82e59dad9d53eddb7693cc93b456d967b492db13
ina <dragoalie@gmail.com>
2021-01-25 00:23:27 +0900

    solved! - use QuickSort with String#compareTo. build the largest number from integer array

--- p0179/Solution.java
+++ p0179/Solution.java
@@ -1,63 +1,65 @@
 package leetcode.p0179;
 
-import java.util.Arrays;
-
 // 1 <= nums.length <= 100
 // 0 <= nums[i] <= 10^9
 class Solution {
   // nums[i] might exceed the range of integer, but still not exceed the range of long.
   public String largestNumber(int[] nums) {
+    quickSort(nums, 0, nums.length - 1);
+    // all-zero array. check after the array is sorted.
+    if (nums[0] == 0) {
+      return "0";
+    }
     StringBuilder res = new StringBuilder();
-    int maxDigit = findMaxDigit(nums);
-    long[][] dummy = new long[nums.length][2];
-
-    int i = 0;
     for (int num : nums) {
-      dummy[i][0] = num;
-      dummy[i][1] = getNewNum(num, maxDigit);
-      i++;
+      res.append(num);
     }
-
-    // reverse order
-    Arrays.sort(dummy, (a, b) -> Long.compare(b[1], a[1]));
-
-    for (long[] a : dummy) {
-      res.append(a[0]);
-    }
-
     return res.toString();
   }
 
-  private int findMaxDigit(int[] nums) {
-    int maxNum = 0;
-    for (int num : nums) {
-      maxNum = Math.max(num, maxNum);
+  private void quickSort(int[] nums,
+                         int l,
+                         int r) {
+    int m = l + (r - l) / 2;
+    int mid = nums[m];
+    int pl = l;
+    int pr = r;
+
+    // do not divide until current pl and pr is crossed completely.
+    while (pl <= pr) {
+      while (isLeftConcatLarger(nums[pl], mid)) {
+        pl++;
+      }
+      while (isLeftConcatLarger(mid, nums[pr])) {
+        pr--;
+      }
+      if (pl <= pr) {
+        swap(nums, pl, pr);
+        // move cursor after swap for setting next position.
+        pl++;
+        pr--;
+      }
     }
-    return getDigit(maxNum);
+    if (l < pr) {
+      quickSort(nums, l, pr);
+    }
+    if (pl < r) {
+      quickSort(nums, pl, r);
+    }
   }
 
-  private long getNewNum(int num, int maxDigit) {
-    final int digit = getDigit(num);
-    if (digit == maxDigit) {
-      return num;
+  private boolean isLeftConcatLarger(int a, int b) {
+    if (a == b) {
+      return false; // since should not move cursors if a and b are equivalent.
     }
-
-    long last = num % 10;
-    long newNum = num;
-    int level = maxDigit - digit;
-    for (int i = 0; i < level; i++) {
-      newNum *= 10;
-      newNum += last;
-    }
-    return newNum;
+    StringBuilder as = new StringBuilder();
+    StringBuilder bs = new StringBuilder();
+    return as.append(a).append(b).toString().compareTo(bs.append(b).append(a).toString()) > -1;
   }
 
-  private int getDigit(int num) {
-    int digit = 0;
-    while (num > 0) {
-      num /= 10;
-      digit++;
-    }
-    return digit;
+  private void swap(int[] nums, int i, int j) {
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
   }
 }
