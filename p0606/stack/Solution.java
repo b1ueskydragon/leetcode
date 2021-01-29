58b2a9f04b28e4b4b26d6a88cd9df0c248fb39ca
ina <dragoalie@gmail.com>
2021-01-29 12:21:54 +0900

    introduce iterative method using stack

---
+++ p0606/stack/Solution.java
@@ -0,0 +1,63 @@
+package leetcode.p0606.stack;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {}
+
+  TreeNode(int val) { this.val = val; }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  public String tree2str(TreeNode t) {
+    if (t == null) {
+      return "";
+    }
+
+    final StringBuilder builder = new StringBuilder();
+    // push right to left
+    final Stack<TreeNode> stack = new Stack<>();
+    final Set<TreeNode> appended = new HashSet<>();
+    stack.push(t);
+
+    while (!stack.isEmpty()) {
+      final TreeNode curr = stack.peek();
+      if (appended.contains(curr)) {
+        stack.pop();
+        builder.append(")"); // close (and pop) after finally returned to original position
+        continue;
+      }
+
+      builder.append("(");
+      builder.append(curr.val);
+      appended.add(curr);
+
+      if (curr.left == null && curr.right != null) {
+        builder.append("()");
+      }
+
+      if (curr.right != null) {
+        stack.push(curr.right);
+      }
+
+      if (curr.left != null) {
+        stack.push(curr.left);
+      }
+    }
+
+    final String str = builder.toString();
+    return str.substring(1, str.length() - 1);
+  }
+}
