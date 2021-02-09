0e4989b5f7b97014e4db0bc6326d5c143185181a
ina <dragoalie@gmail.com>
2021-02-09 18:49:36 +0900

    solved - Preprocessing to narrows the range before enter the Backtracking. maximum product of three numbers

--- p0628/backtracking/Solution.java
+++ p0628/backtracking/Solution.java
@@ -1,16 +1,32 @@
 package leetcode.p0628.backtracking;
 
+import java.util.Arrays;
+
 class Solution {
   public int maximumProduct(int[] nums) {
-    int res = Integer.MIN_VALUE; // for the array which only has negative number
-    for (int num : nums) {
-      if (num > 0) {
-        res = 1;
-        break;
-      }
+    int res = Integer.MIN_VALUE;
+
+    if (nums.length < 6) {
+      return maximumProduct(nums, 3, 0, 1, res);
     }
 
-    return maximumProduct(nums, 3, 0, 1, res);
+    Arrays.sort(nums);
+
+    final int last = nums.length - 1;
+
+    // top 3 abs (from right)
+    final int a = nums[last];
+    final int b = nums[last - 1];
+    final int c = nums[last - 2];
+
+    // top 3 abs (from left)
+    final int x = nums[0];
+    final int y = nums[1];
+    final int z = nums[2];
+
+    final int[] cands = {a, b, c, x, y, z};
+    return maximumProduct(cands, 3, 0, 1, res);
+
   }
 
   // backtracking
@@ -24,10 +40,9 @@ class Solution {
     }
 
     for (int i = pos; i < nums.length; i++) {
-      // return and make a different choice
+      // return and make a "different" choice
       res = maximumProduct(nums, k - 1, i + 1, acc * nums[i], res);
     }
-
     return res;
   }
 }
