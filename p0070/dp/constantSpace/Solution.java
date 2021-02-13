febbf187adfc660896319063a62f41f9ba9902a6
ina <dragoalie@gmail.com>
2021-02-13 19:43:58 +0900

    solved - DP Constant space. calc n from previous n-1 and n-2 (Reuse previous results). Climbing Stairs 1 or 2 steps

---
+++ p0070/dp/constantSpace/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.p0070.dp.constantSpace;
+
+class Solution {
+  public int climbStairs(int n) {
+    int prevOneStep = 1;
+    int prevTwoSteps = 1;
+    int soFar = 1;
+
+    for (int i = 2; i <= n; i++) {
+      soFar = prevOneStep + prevTwoSteps;
+      prevTwoSteps = prevOneStep;
+      prevOneStep = soFar;
+    }
+    return soFar;
+  }
+}
