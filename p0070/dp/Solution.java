2cf028dcfa0928fd4e0f2bed95a10c9f4bab3681
ina <dragoalie@gmail.com>
2021-02-13 18:54:17 +0900

    solved - DP. calc n from previous n-1 and n-2 (Reuse previous results). Climbing Stairs 1 or 2 steps

---
+++ p0070/dp/Solution.java
@@ -0,0 +1,13 @@
+package leetcode.p0070.dp;
+
+class Solution {
+  public int climbStairs(int n) {
+    final int[] stepsSoFar = new int[n + 1];
+    stepsSoFar[0] = 1; // sentinel
+    stepsSoFar[1] = 1;
+    for (int i = 2; i <= n; i++) {
+      stepsSoFar[i] = stepsSoFar[i - 1] + stepsSoFar[i - 2];
+    }
+    return stepsSoFar[n];
+  }
+}
