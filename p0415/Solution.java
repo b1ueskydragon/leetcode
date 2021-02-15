8fa319c288f016d445b217d76c0e1dd0a0e1424d
ina <dragoalie@gmail.com>
2021-02-15 17:15:58 +0900

    improve - without using StringBuilder to zero-padding. Add Strings

--- p0415/Solution.java
+++ p0415/Solution.java
@@ -2,39 +2,28 @@ package leetcode.p0415;
 
 class Solution {
   public String addStrings(String num1, String num2) {
-    int n1 = num1.length();
-    int n2 = num2.length();
-
-    final StringBuilder sb2 = new StringBuilder(num2).reverse();
-    while (n1 > n2) {
-      sb2.append("0");
-      n2++;
-    }
-
-    final StringBuilder sb1 = new StringBuilder(num1).reverse();
-    while (n2 > n1) {
-      sb1.append("0");
-      n1++;
-    }
-
-    final char[] xs = sb1.toString().toCharArray();
-    final char[] ys = sb2.toString().toCharArray();
+    int n1 = num1.length() - 1;
+    int n2 = num2.length() - 1;
     final StringBuilder res = new StringBuilder();
 
     int cache = 0;
-    // keeping reverse order
-    for (int i = 0; i < n1; i++) {
-      int acc = (xs[i] + ys[i]) % 96;
+    while (n1 >= 0 || n2 >= 0) {
+      // '0' == 48
+      int x = (n1 >= 0) ? num1.charAt(n1) - 48 : 0;
+      int y = (n2 >= 0) ? num2.charAt(n2) - 48 : 0;
+      int acc = x + y;
       int base = ((acc >= 10) ? acc - 10 : acc) + cache;
 
       // set a cache after the current base has been appended
       if (base >= 10) {
         res.append(base - 10);
         cache = 1;
-        continue;
+      } else {
+        res.append(base);
+        cache = (acc >= 10) ? 1 : 0;
       }
-      res.append(base);
-      cache = (acc >= 10) ? 1 : 0;
+      n1--;
+      n2--;
     }
 
     if (cache == 1) {
