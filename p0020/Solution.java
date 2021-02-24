bf9833ed6d4fadbf8758b89feef9a966222817b6
ina <dragoalie@gmail.com>
2021-02-24 11:16:37 +0900

    refactor - replace to charAt. Check the brackets are balanced (well-formed)

--- p0020/Solution.java
+++ p0020/Solution.java
@@ -1,32 +1,31 @@
 package leetcode.p0020;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
 class Solution {
   public boolean isValid(String s) {
-    final Map<Character, Character> table = new HashMap<>();
-    table.put('(', ')');
-    table.put('[', ']');
-    table.put('{', '}');
-
     final Stack<Character> stack = new Stack<>();
-    for (char c : s.toCharArray()) {
-      if (table.containsKey(c)) {
-        stack.push(c);
+    final Map<Character, Character> pairs =
+        Map.of('(', ')',
+            '{', '}',
+            '[', ']');
+    int i = 0;
+    while (i < s.length()) {
+      final char x = s.charAt(i++);
+      if (pairs.containsKey(x)) {
+        stack.push(x);
         continue;
       }
-
-      // since we don't push ')' ']' '}' in the stack.
+      // there's no open-bracket to pop even the close-bracket arrived
       if (stack.isEmpty()) {
         return false;
       }
-
-      if (c != table.get(stack.pop())) {
+      if (pairs.get(stack.pop()) != x) {
         return false;
       }
     }
+    // all of open-brackets should be popped
     return stack.isEmpty();
   }
 }
