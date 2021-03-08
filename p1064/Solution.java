951ba8588326439a8fef2e5301b79fa4ec135de9
ina <dragoalie@gmail.com>
2021-03-08 11:13:48 +0900

    sovled - Binary Search. find Fixed Point from sorted array of distinct elements

---
+++ p1064/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.p1064;
+
+/**
+ * Given an array A of distinct integers sorted in ascending order, return the smallest index i that satisfies A[i] == i.
+ * Return -1 if no such i exists.
+ */
+public class Solution {
+
+  public int fixedPoint(int[] sortedAscArray) {
+    int l = 0;
+    int r = sortedAscArray.length - 1;
+
+    while (l <= r) {
+      int pi = l + (r - l) / 2; // left-align pivot
+      int pv = sortedAscArray[pi];
+
+      if (pi == pv) {
+        return pi;
+      }
+
+      if (pi < pv) {
+        r = pi - 1;
+        continue;
+      }
+
+      l = pi + 1;
+    }
+
+    return -1;
+  }
+}
