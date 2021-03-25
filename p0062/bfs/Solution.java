ebc49b62139ed54d785e119532b5176729a3fe47
ina <dragoalie@gmail.com>
2021-03-25 23:50:33 +0900

    solved - Dynamic programming. just accumulate the previous results. unique paths

--- p0062/bfs/Solution.java
+++ p0062/bfs/Solution.java
@@ -1,47 +1,16 @@
 package leetcode.p0062.bfs;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
   public int uniquePaths(int m, int n) {
-    final Queue<Point> queue = new LinkedList<>();
-    queue.add(new Point(0, 0));
+    // accumulated count to reach the point so far
+    final int[][] steps = new int[m][n];
 
-    // vectors
-    final int[] goRight = {0, 1};
-    final int[] goDown = {1, 0};
-
-    int count = 0;
-
-    while (!queue.isEmpty()) {
-      final var parent = queue.poll();
-      final int x = parent.m;
-      final int y = parent.n;
-
-      if (x >= m || y >= n) {
-        continue;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        // If encounter the edge, put 1. others, prevUp + prevLeft
+        steps[i][j] = (i == 0 || j == 0) ? 1 : steps[i - 1][j] + steps[i][j - 1];
       }
-
-      // encounter the Finish
-      if (x == m - 1 && y == n - 1) {
-        count++;
-      }
-
-      queue.add(new Point(x + goRight[0], y + goRight[1]));
-      queue.add(new Point(x + goDown[0], y + goDown[1]));
     }
-
-    return count;
-  }
-
-  static class Point {
-    int m;
-    int n;
-
-    Point(int m, int n) {
-      this.m = m;
-      this.n = n;
-    }
+    return steps[m - 1][n - 1];
   }
 }
