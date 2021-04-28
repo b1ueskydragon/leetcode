f193b7f2d4962f9df513e80e73014d3e7ac0412b
ina <dragoalie@gmail.com>
2021-04-28 11:39:22 +0900

    solved again - Find a middle point and compare each half. Palindrome Linked List

---
+++ p0234/findMid/_1/Solution.java
@@ -0,0 +1,59 @@
+package leetcode.p0234.findMid._1;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val) { this.val = val; }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Solution {
+  public boolean isPalindrome(ListNode head) {
+    ListNode last = head;
+    int size = 0;
+    while (last.next != null) {
+      last = last.next;
+      size++;
+    }
+
+    ListNode left = head;
+    ListNode prev = null;
+    ListNode right;
+
+    int m = size / 2;
+    while (m > 0) {
+      right = left.next;
+      left.next = prev;
+      prev = left;
+      left = right;
+      m--;
+    }
+
+    right = left.next;
+
+    if (size % 2 == 0) {
+      // odd-len nodes
+      left = prev;
+    } else {
+      // even-len nodes
+      left.next = prev;
+    }
+
+    while (left != null && right != null) {
+      if (left.val != right.val) {
+        return false;
+      }
+      left = left.next;
+      right = right.next;
+    }
+
+    return true;
+  }
+}
