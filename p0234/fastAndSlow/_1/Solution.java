1409239572b02ccd6f7442d51e422af9d4d2d727
ina <dragoalie@gmail.com>
2021-05-02 00:08:27 +0900

    solved again - Find a middle point with two cursors with different speeds. Palindrome Linked List

---
+++ p0234/fastAndSlow/_1/Solution.java
@@ -0,0 +1,48 @@
+package leetcode.p0234.fastAndSlow._1;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val) { this.val = val; }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Solution {
+  public boolean isPalindrome(ListNode head) {
+    ListNode fast = head;
+    ListNode slow = head;
+    ListNode mid = null;
+    boolean hasEvenSize = true;
+
+    while (fast != null && fast.next != null) {
+      if (fast.next.next == null) {
+        hasEvenSize = false;
+      }
+      fast = fast.next.next;
+      ListNode next = slow.next;
+      slow.next = mid;
+      mid = slow;
+      slow = next;
+    }
+
+    if (hasEvenSize) {
+      slow = slow.next;
+    }
+
+    while (slow != null && mid != null) {
+      if (slow.val != mid.val) {
+        return false;
+      }
+      slow = slow.next;
+      mid = mid.next;
+    }
+    return true;
+  }
+}
