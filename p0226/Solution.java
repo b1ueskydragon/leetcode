c46d5f43c6ddca235c86314ae6660e6c9849535d
ina <dragoalie@gmail.com>
2021-05-10 17:46:06 +0900

    solved - DFS recursive. Invert Binary Tree

---
+++ p0226/Solution.java
@@ -0,0 +1,34 @@
+package leetcode.p0226;
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {}
+
+  TreeNode(int val) { this.val = val; }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+      return root;
+    }
+
+    TreeNode tmp = root.right;
+    root.right = root.left;
+    root.left = tmp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+  }
+}
