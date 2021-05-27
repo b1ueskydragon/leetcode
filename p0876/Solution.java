9ed4e3ce7667f7f6449a39890e75433223ca7231
ina <dragoalie@gmail.com>
2021-05-27 23:39:05 +0900

    solved - fast and slow pointers. Middle of the Linked List

---
+++ p0876/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.p0876;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val) { this.val = val; }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Solution {
+  public ListNode middleNode(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+
+    return slow;
+  }
+}
