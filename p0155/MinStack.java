525833d8e15e281a923bb976f6dc36aeb520df3d
ina <dragoalie@gmail.com>
2021-07-14 00:24:00 +0900

    solved - Impl by LinkedList of value <-> current stage's min pair Node. Min Stack

---
+++ p0155/MinStack.java
@@ -0,0 +1,55 @@
+package leetcode.p0155;
+
+import java.util.LinkedList;
+
+class MinStack {
+
+  private final LinkedList<Node> nodes;
+
+  private int min;
+
+  /**
+   * initialize your data structure here.
+   */
+  public MinStack() {
+    nodes = new LinkedList<>();
+    min = Integer.MAX_VALUE;
+  }
+
+  public void push(int val) {
+    min = Math.min(min, val);
+    nodes.addLast(new Node(val, min));
+  }
+
+  public void pop() {
+    nodes.removeLast();
+    min = (nodes.isEmpty()) ? Integer.MAX_VALUE : nodes.getLast().min;
+  }
+
+  public int top() {
+    return nodes.getLast().val;
+  }
+
+  public int getMin() {
+    return this.min;
+  }
+
+  static class Node {
+    private int val;
+    private int min; // min at the current stack stage
+
+    private Node(int val, int min) {
+      this.val = val;
+      this.min = min;
+    }
+  }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
