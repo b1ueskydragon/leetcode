04852555a5a9a046494400c43f4ba3abaef17861
ina <dragoalie@gmail.com>
2021-08-13 23:39:34 +0900

    refactor - simplify. use Hashtable. O(N) space, O(N) time. Intersection of Two Linked Lists

--- p0160/hashtable/Solution.java
+++ p0160/hashtable/Solution.java
@@ -14,40 +14,21 @@ class ListNode {
 }
 
 public class Solution {
-  // Make a cycle intentionally. Straight-forward, O(N) space, O(N) time.
+  // Straight-forward, O(N) space, O(N) time.
   public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-    ListNode a = headA;
-    ListNode b = headB;
-
-    int aLen = 1;
-    while (a.next != null) {
-      a = a.next;
-      aLen++;
-    }
-
-    int bLen = 1;
-    while (b.next != null) {
-      b = b.next;
-      bLen++;
-    }
-
-    ListNode aEnd = a;
-    a.next = headB;
-
     final Set<ListNode> checked = new HashSet<>();
 
-    int i = 0;
-    while (i < aLen + bLen) {
-      if (checked.contains(headA)) {
-        aEnd.next = null;
-        return headA;
-      }
+    while (headA != null) {
       checked.add(headA);
       headA = headA.next;
-      i++;
     }
 
-    aEnd.next = null;
+    while (headB != null) {
+      if (checked.contains(headB)) {
+        return headB;
+      }
+      headB = headB.next;
+    }
 
     return null;
   }
