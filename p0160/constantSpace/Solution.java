034e6c9ffbe37622fc1beb3c4fddec8871210f98
ina <dragoalie@gmail.com>
2021-08-14 14:55:19 +0900

    solved - get a diff of two lengths and align start points to find an intersection. Intersection of Two Linked Lists

---
+++ p0160/constantSpace/Solution.java
@@ -0,0 +1,49 @@
+package leetcode.p0160.constantSpace;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
+}
+
+public class Solution {
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    ListNode a = headA;
+    ListNode b = headB;
+
+    int lenA = 0;
+    while (a != null) {
+      lenA++;
+      a = a.next;
+    }
+
+    int lenB = 0;
+    while (b != null) {
+      lenB++;
+      b = b.next;
+    }
+
+    int diff = Math.abs(lenA - lenB);
+
+    // Align start points
+    while (diff > 0) {
+      if (lenA > lenB) {
+        headA = headA.next;
+      } else {
+        headB = headB.next;
+      }
+      diff--;
+    }
+
+    while (headA != headB) {
+      headA = headA.next;
+      headB = headB.next;
+    }
+
+    return headA; // or headB
+  }
+}
