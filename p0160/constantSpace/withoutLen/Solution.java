c2a38483ccfb1c71d9a192c41c192bdc6192f0e1
ina <dragoalie@gmail.com>
2021-08-19 07:14:30 +0900

    Add more comments to prove

--- p0160/constantSpace/withoutLen/Solution.java
+++ p0160/constantSpace/withoutLen/Solution.java
@@ -17,6 +17,15 @@ public class Solution {
 
     // The shorter one can occupy the longer head first and move forward first in the next loop,
     // so our two points will meet in the intersection point in the future.
+    //
+    // A len = a + c
+    // B len = b + c
+    //
+    // Pointer A moves
+    //    a + c + b + c (b + c moves after switching pointer)
+    // Pointer B moves
+    //    b + c + a + c (a + c moves after switching pointer)
+    //  so Pointer A and B meet after a + c + b (b + c + a) steps.
     while (a != b) {
       a = (a == null) ? headB : a.next;
       b = (b == null) ? headA : b.next;
