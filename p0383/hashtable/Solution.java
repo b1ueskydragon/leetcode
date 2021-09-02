b48137fbf99e3f9ecf9f5091abecb5dd09a2b37a
ina <dragoalie@gmail.com>
2021-09-02 20:39:23 +0900

    improve - make an early return for invalid input. Ransom Note

--- p0383/hashtable/Solution.java
+++ p0383/hashtable/Solution.java
@@ -6,13 +6,20 @@ import java.util.Map;
 class Solution {
   public boolean canConstruct(String ransomNote, String magazine) {
     final Map<Character, Integer> countTable = new HashMap<>();
-    for (int i = 0; i < magazine.length(); i++) {
+    final int m = magazine.length();
+    final int n = ransomNote.length();
+
+    if (n > m) {
+      return false;
+    }
+
+    for (int i = 0; i < m; i++) {
       char x = magazine.charAt(i);
       countTable.putIfAbsent(x, 0);
       countTable.put(x, countTable.get(x) + 1);
     }
 
-    for (int i = 0; i < ransomNote.length(); i++) {
+    for (int i = 0; i < n; i++) {
       char x = ransomNote.charAt(i);
       if (!countTable.containsKey(x)) {
         return false;
