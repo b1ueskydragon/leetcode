f4e98b63ac7235e716265f12ccd2da7b8b8719ac
ina <dragoalie@gmail.com>
2021-09-16 10:32:17 +0900

    solved - using division, O(1) space and O(N) time. Product of Array Except Self

---
+++ p0238/division/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.p0238.division;
+
+class Solution {
+  // using the division op
+  // O(1) space, O(N) time
+  public int[] productExceptSelf(int[] nums) {
+    int mult = 1;
+    int zeros = 0;
+    for (int num : nums) {
+      if (num == 0) {
+        zeros++;
+        continue;
+      }
+      mult *= num;
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      if (zeros > 1) {
+        nums[i] = 0;
+        continue;
+      }
+      if (zeros == 0) {
+        nums[i] = mult / nums[i];
+        continue;
+      }
+      nums[i] = (nums[i] == 0) ? mult : 0;
+    }
+
+    return nums;
+  }
+}
