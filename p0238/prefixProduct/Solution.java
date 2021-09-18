448ee30d8e982e998a4eb2e4836b8fdfd6484989
ina <dragoalie@gmail.com>
2021-09-18 17:31:14 +0900

    introduce prefix-suffix product. Product of Array Except Self

---
+++ p0238/prefixProduct/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.p0238.prefixProduct;
+
+class Solution {
+  // without using the division op
+  // O(N) time, O(1) extra space (output array does not count as extra space)
+  public int[] productExceptSelf(int[] nums) {
+    final int n = nums.length;
+    final int[] res = new int[n];
+    res[0] = 1;
+
+    // left to right
+    for (int i = 1; i < n; i++) {
+      res[i] = res[i - 1] * nums[i - 1];
+    }
+
+    // right to left
+    int acc = 1;
+    for (int i = n - 1; i >= 0; i--) {
+      res[i] *= acc;
+      acc *= nums[i];
+    }
+
+    return res;
+  }
+}
