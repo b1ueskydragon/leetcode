7b060afa39d956fc5cb660d448de62dcbd6db532
ina <dragoalie@gmail.com>
2021-09-18 17:00:48 +0900

    solved - without using division, but TLE. Product of Array Except Self

---
+++ p0238/TLE/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.p0238.TLE;
+
+class Solution {
+  // without using the division op
+  public int[] productExceptSelf(int[] nums) {
+    final int n = nums.length;
+
+    final int[] res = new int[n];
+    for (int i = 0; i < n; i++) {
+      res[i] = 1;
+    }
+
+    int k = 1;
+    while (k < n) {
+      for (int i = 0; i < n; i++) {
+        res[i] *= nums[(i + k) % n];
+      }
+      k++;
+    }
+
+    return res;
+  }
+}
