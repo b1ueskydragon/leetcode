a0cf439500e9404cb073339f99cec8c719004a04
ina <dragoalie@gmail.com>
2021-12-05 23:55:50 +0900

    solved - using DFS. memoing the longest diameter so far on each node. Diameter of Binary Tree

---
+++ p0543/dfs_memo/Solution.java
@@ -0,0 +1,39 @@
+package leetcode.p0543.dfs_memo;
+
+class Solution {
+  private int maxDiameter = -1;
+
+  //  This path may or may not pass through the root.
+  public int diameterOfBinaryTree(TreeNode root) {
+    calcMaxDiameter(root);
+    return maxDiameter;
+  }
+
+  private int calcMaxDiameter(TreeNode node) {
+    if (node == null) {
+      return -1;
+    }
+    final int leftSoFar = calcMaxDiameter(node.left) + 1;
+    final int rightSoFar = calcMaxDiameter(node.right) + 1;
+
+    maxDiameter = Math.max(maxDiameter, leftSoFar + rightSoFar);
+
+    return Math.max(leftSoFar, rightSoFar);
+  }
+}
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {}
+
+  TreeNode(int val) {this.val = val;}
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
