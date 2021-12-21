b025232eb42f2e9caf2657705b8423a416f5273f
ina <dragoalie@gmail.com>
2021-12-21 16:48:13 +0900

    solved - recursive. make the numbers smaller properly. Ugly Number

---
+++ p0263/recursive/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.p0263.recursive;
+
+class Solution {
+  public boolean isUgly(int n) {
+    if (n <= 0) return false;
+    if (n < 7) return true;
+    if (n % 2 == 0) return isUgly(n / 2);
+    if (n % 3 == 0) return isUgly(n / 3);
+    if (n % 5 == 0) return isUgly(n / 5);
+    return false;
+  }
+}
