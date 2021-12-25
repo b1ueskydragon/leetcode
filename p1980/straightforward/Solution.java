f41519578a54ccb06830d25368acd65d4b547c98
ina <dragoalie@gmail.com>
2021-12-25 11:26:46 +0900

    solved - but a straightforward way.. - Find Unique Binary String

---
+++ p1980/straightforward/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.p1980.straightforward;
+
+// TODO Backtracking
+class Solution {
+  public String findDifferentBinaryString(String[] nums) {
+    // nums containing n unique binary strings, n == nums.length
+    final int n = nums.length;
+    final boolean[] dict = new boolean[1 << n];
+    for (String str : nums) {
+      dict[Integer.parseInt(str, 2)] = true;
+    }
+    for (int i = 0; i < dict.length; i++) {
+      if (!dict[i]) return String.valueOf(String.format("%0" + n + "d", Integer.valueOf(Integer.toBinaryString(i))));
+    }
+    return "";
+  }
+}
