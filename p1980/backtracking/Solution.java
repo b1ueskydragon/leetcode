f9f9f5c766c20f6f3988961de6f2d8ae70a78ab6
ina <dragoalie@gmail.com>
2021-12-31 10:40:45 +0900

    refactor - replace to Arrays#fill, make return type to void

--- p1980/backtracking/Solution.java
+++ p1980/backtracking/Solution.java
@@ -1,7 +1,6 @@
 package leetcode.p1980.backtracking;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
@@ -10,43 +9,34 @@ class Solution {
   public String findDifferentBinaryString(String[] nums) {
     final int n = nums.length;
     final char[] init = new char[n];
-    for (int i = 0; i < n; i++) {
-      init[i] = '0';
-    }
-    final Set<String> table = new HashSet<>() {{
-      addAll(Arrays.asList(nums));
-    }};
-    generateAndMatch(0, 0, n, init, table);
+    Arrays.fill(init, '0');
+    generateAndMatch(0, 0, n, init, Set.copyOf(Arrays.asList(nums)));
     return str;
   }
 
   // generate a single element and match
-  private char[] generateAndMatch(int k,
-                                  int p,
-                                  int n,
-                                  char[] curr,
-                                  Set<String> table) {
+  private void generateAndMatch(int k,
+                                int p,
+                                int n,
+                                char[] curr,
+                                Set<String> table) {
     final String s = String.valueOf(curr);
     // make an early return
     if (!table.contains(s)) {
       str = s;
-      return curr;
+      return;
     }
     if (k == n) {
-      return curr;
+      return;
     }
 
     for (int i = p; i < n; i++) {
       final char[] next = Arrays.copyOf(curr, n);
-
       // bit reverse
       // '0' ^ 1 = '1', '1' ^ 1 = '0'
       next[i] ^= 1;
-
       // start from next horizontal level (i + 1)
       generateAndMatch(k + 1, i + 1, n, next, table);
     }
-
-    return curr;
   }
 }
