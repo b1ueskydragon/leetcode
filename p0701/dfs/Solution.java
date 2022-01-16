550e7f8571ead515bf6aaedb9ae48baf8eec0685
ina <dragoalie@gmail.com>
2022-01-16 12:52:08 +0900

    Add comments

--- p0701/dfs/Solution.java
+++ p0701/dfs/Solution.java
@@ -18,9 +18,11 @@ class TreeNode {
 
 class Solution {
   public TreeNode insertIntoBST(TreeNode root, int val) {
+    // Create a new node at this point
     if (root == null) {
       return new TreeNode(val);
     }
+    // Append the node to a current level root after breaking out of recursion
     if (root.val > val) {
       root.left = insertIntoBST(root.left, val);
     } else {
