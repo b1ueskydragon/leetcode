ef73b4848caf6b6603b9b2823cb1c7b2ef3a77eb
ina <dragoalie@gmail.com>
2022-04-07 02:06:27 +0900

    solved - use a property of BTS. DFS recursion based on side effects. Minimum Absolute Difference in BST

---
+++ p0530/dfs/Solution.java
@@ -0,0 +1,41 @@
+package leetcode.p0530.dfs;
+
+class Solution {
+    private int prev = 10_001;
+    private int minDiff = 10_001;
+
+    // Use Binary Search Tree property which guarantees left <= right
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) {
+            return minDiff;
+        }
+
+        getMinimumDifference(root.left);
+
+        minDiff = Math.min(minDiff, Math.abs(root.val - prev));
+        prev = root.val;
+
+        getMinimumDifference(root.right);
+
+        return minDiff;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
