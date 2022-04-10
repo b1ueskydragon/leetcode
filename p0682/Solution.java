bf20f27f9ed4075ee2b61be9b35e6132673ecba6
ina <dragoalie@gmail.com>
2022-04-10 18:08:48 +0900

    refactor - separate to functions

--- p0682/Solution.java
+++ p0682/Solution.java
@@ -9,31 +9,45 @@ class Solution {
         int acc = 0; // to reduce an operation of sum all
         for (String v : ops) {
             switch (v) {
-                // It is guaranteed there will always be a previous score
                 case "D":
-                    dq.addLast(dq.peekLast() * 2);
-                    acc += dq.peekLast();
+                    acc += operateD(dq);
                     continue;
-                    // It is guaranteed there will always be a previous score
                 case "C":
-                    final int x = dq.pollLast();
-                    acc -= x;
+                    acc -= operateC(dq);
                     continue;
-                    // It is guaranteed there will always be two previous scores
                 case "+":
-                    final int prev1 = dq.pollLast();
-                    final int prev2 = dq.pollLast();
-                    dq.addLast(prev2);
-                    dq.addLast(prev1);
-                    dq.addLast(prev1 + prev2);
-                    acc += dq.peekLast();
+                    acc += operatePlus(dq);
                     continue;
-                    // This wil be a numeric value
-                default:
-                    dq.addLast(Integer.valueOf(v));
-                    acc += dq.peekLast();
+                default: // This wil be a numeric value 
+                    acc += operateNum(dq, v);
             }
         }
         return acc;
+    }
+
+    // It is guaranteed there will always be a previous score
+    private static int operateD(Deque<Integer> dq) {
+        dq.addLast(dq.peekLast() * 2);
+        return dq.peekLast();
+    }
+
+    // It is guaranteed there will always be a previous score    
+    private static int operateC(Deque<Integer> dq) {
+        return dq.pollLast();
+    }
+
+    // It is guaranteed there will always be two previous scores                       
+    private static int operatePlus(Deque<Integer> dq) {
+        final int prev1 = dq.pollLast();
+        final int prev2 = dq.pollLast();
+        dq.addLast(prev2);
+        dq.addLast(prev1);
+        dq.addLast(prev1 + prev2);
+        return dq.peekLast();
+    }
+
+    private static int operateNum(Deque<Integer> dq, String v) {
+        dq.addLast(Integer.valueOf(v));
+        return dq.peekLast();
     }
 }
