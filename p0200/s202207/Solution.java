6ef221dbdd862fd313efc2efa7553b61a6c2e0d4
ina <dragoalie@gmail.com>
2022-07-20 17:32:46 +0900

    refactor - abolish to use an additional array. Number of Islands

--- p0200/s202207/Solution.java
+++ p0200/s202207/Solution.java
@@ -13,28 +13,22 @@ class Solution {
     public int numIslands(char[][] grid) {
         final int m = grid.length;
         final int n = grid[0].length;
-        final boolean[][] visited = new boolean[m][n];
 
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
-                if (visited[r][c]) {
+                if (grid[r][c] == '0') {
                     continue;
                 }
-                visitAll(grid, visited, m, n, r, c);
-                visited[r][c] = true;
+                visitAll(grid, m, n, r, c);
+                grid[r][c] = '0'; // mark as visited
             }
         }
         return islands;
     }
 
     private void visitAll(char[][] grid,
-                          boolean[][] visited,
                           int m, int n,
                           int r, int c) {
-        if (grid[r][c] == '0') {
-            return;
-        }
-
         final Deque<int[]> queue = new ArrayDeque<>();
         queue.addLast(new int[]{r, c});
 
@@ -43,16 +37,16 @@ class Solution {
             for (int i = 0; i < 4; i++) {
                 final int x = curr[0] + vectorR[i];
                 final int y = curr[1] + vectorC[i];
-                if (isValidPos(x, y, m, n) && !visited[x][y] && grid[x][y] == '1') {
+                if (isValidPos(x, y, m, n) && grid[x][y] == '1') {
                     queue.addLast(new int[]{x, y});
-                    visited[x][y] = true;
+                    grid[x][y] = '0'; // mark as visited
                 }
             }
         }
         islands++;
     }
 
-    private boolean isValidPos(int x, int y, int m, int n) {
+    private static boolean isValidPos(int x, int y, int m, int n) {
         return x >= 0 && y >= 0 && x < m && y < n;
     }
 }
