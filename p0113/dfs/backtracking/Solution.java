94f30f2a914a7d07dbeea62f6d2548e44af43aca
ina <dragoalie@gmail.com>
2022-09-24 12:56:10 +0900

    solved - keep the current path in the backtracking-ish recursion. Path Sum II

---
+++ p0113/dfs/backtracking/Solution.java
@@ -0,0 +1,58 @@
+package leetcode.p0113.dfs.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        return pathSum(root, targetSum, new ArrayList<>(), new ArrayList<>());
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root,
+                                       int targetSum,
+                                       List<List<Integer>> paths,
+                                       List<Integer> path) {
+        if (root == null) {
+            return paths;
+        }
+
+        final int currStageVal = root.val;
+
+        // keep paths so far (current status) for each node.
+        // each node will keep its own path list.
+        final List<Integer> currStagePath = new ArrayList<>(path);
+        currStagePath.add(currStageVal);
+
+        if (root.left == null && root.right == null) {
+            if (targetSum == currStageVal) {
+                paths.add(currStagePath);
+            }
+            return paths;
+        }
+
+        pathSum(root.left, targetSum - currStageVal, paths, currStagePath);
+        pathSum(root.right, targetSum - currStageVal, paths, currStagePath);
+
+        return paths;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
