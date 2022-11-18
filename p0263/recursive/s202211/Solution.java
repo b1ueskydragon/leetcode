fd1c31b9170ca846f449c95d12bd66311eecf862
ina <dragoalie@gmail.com>
2022-11-18 14:51:47 +0900

    solved - using recursive conditions with a base condition. Ugly Number

---
+++ p0263/recursive/s202211/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.p0263.recursive.s202211;
+
+class Solution {
+    public boolean isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        if (n == 1 || n == 2 || n == 3 || n == 5) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return isUgly(n / 2);
+        }
+        if (n % 3 == 0) {
+            return isUgly(n / 3);
+        }
+        if (n % 5 == 0) {
+            return isUgly(n / 5);
+        }
+        return false;
+    }
+}
