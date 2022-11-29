f43ec977832336e9cedb35c059a4b2879de56ee7
ina <dragoalie@gmail.com>
2022-11-29 15:54:06 +0900

    improve - use other efficient methods of ArrayList to keep same element between the HashMap. Insert Delete GetRandom O(1)

--- p0380/Solution.java
+++ p0380/Solution.java
@@ -1,54 +1,63 @@
 package leetcode.p0380;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 // Each function works in O(1) time complexity
 // There will be at least one element in the data structure when getRandom is called
 class RandomizedSet {
-    private final Set<Integer> mainSet;
 
-    // tracking
-    // backUp only interested in adding elements
-    // this allowing duplications. if the number added repeatedly, a probability of selection may increase
-    private final List<Integer> backUp;
-    private final Set<Integer> removed;
+    // map val and location (position) on the list
+    private final Map<Integer, Integer> map;
+    // to get a random index. this keeps same elements between the map
+    private final ArrayList<Integer> list;
 
     private final ThreadLocalRandom rand;
 
     public RandomizedSet() {
-        mainSet = new HashSet<>();
-        backUp = new ArrayList<>();
-        removed = new HashSet<>();
+        map = new HashMap<>();
+        list = new ArrayList<>();
         rand = ThreadLocalRandom.current();
     }
 
     public boolean insert(int val) {
-        final boolean notExists = mainSet.add(val);
-        if (notExists) {
-            backUp.add(val);
+        if (map.containsKey(val)) {
+            // nothing to add
+            return false;
         }
-        removed.remove(val);
-        return notExists;
+        final int index = list.size();
+        map.put(val, index);
+        list.add(val);
+        return true;
     }
 
+    // move the removing element to the last
     public boolean remove(int val) {
-        final boolean exists = mainSet.remove(val);
-        if (exists) {
-            removed.add(val);
+        if (!map.containsKey(val)) {
+            // nothing to remove
+            return false;
         }
-        return exists;
+        final int index = map.get(val);
+        final int last = list.size() - 1;
+        if (index < last) {
+            // swap the last and the current
+            // order is not matter since all elements are get randomly
+            // arraylist's #set #get work in O(1) time complexity
+            final int currLastVal = list.get(last);
+            list.set(index, currLastVal);
+            map.put(currLastVal, index);
+        }
+        map.remove(val);
+        // arraylist's removing last elements only takes O(1) time complexity
+        // since 'System#arraycopy will be skipped
+        list.remove(last);
+        return true;
     }
 
     public int getRandom() {
-        final int maxBound = backUp.size();
-        Integer cand = backUp.get(rand.nextInt(maxBound));
-        while (removed.contains(cand)) {
-            cand = backUp.get(rand.nextInt(maxBound));
-        }
-        return cand;
+        final int maxBound = list.size();
+        return list.get(rand.nextInt(maxBound));
     }
 }
