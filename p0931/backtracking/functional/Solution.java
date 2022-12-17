39b9ac4ceec71fa79ef340c1249a32b96a82f892
ina <dragoalie@gmail.com>
2022-12-18 00:19:55 +0900

    introduce a cache to reduce redundant vertical scanning. Minimum Falling Path Sum

--- p0931/backtracking/functional/Solution.java
+++ p0931/backtracking/functional/Solution.java
@@ -4,9 +4,13 @@ package leetcode.p0931.backtracking.functional;
 // 1 <= n <= 100
 // -100 <= matrix[i][j] <= 100
 class Solution {
+    // consider a negative value (initial value should not be zero)
+    private Integer[][] cache;
+
     public int minFallingPathSum(int[][] matrix) {
         final int n = matrix.length;
         int minSumSoFar = 10001;
+        cache = new Integer[n][n];
         // horizontal scanning
         for (int i = 0; i < n; i++) {
             minSumSoFar = Math.min(minSumSoFar, verticalVisit(matrix, 0, i));
@@ -23,22 +27,31 @@ class Solution {
             // the top row is a starting point of vertical accumulation
             return matrix[x][y];
         }
+        if (cache[x][y] != null) {
+            return cache[x][y];
+        }
         if (y == 0) {
             // cannot go to the left
             int bottom = verticalVisit(matrix, x + 1, y);
             int bottomRight = verticalVisit(matrix, x + 1, y + 1);
-            return Math.min(bottom, bottomRight) + matrix[x][y];
+            int minSumSoFar = Math.min(bottom, bottomRight) + matrix[x][y];
+            cache[x][y] = minSumSoFar;
+            return minSumSoFar;
         }
         if (y == matrix.length - 1) {
             // cannot go to the right
             int bottom = verticalVisit(matrix, x + 1, y);
             int bottomLeft = verticalVisit(matrix, x + 1, y - 1);
-            return Math.min(bottom, bottomLeft) + matrix[x][y];
+            int minSumSoFar = Math.min(bottom, bottomLeft) + matrix[x][y];
+            cache[x][y] = minSumSoFar;
+            return minSumSoFar;
         }
         // can go anywhere
         int bottom = verticalVisit(matrix, x + 1, y);
         int bottomRight = verticalVisit(matrix, x + 1, y + 1);
         int bottomLeft = verticalVisit(matrix, x + 1, y - 1);
-        return Math.min(bottom, Math.min(bottomLeft, bottomRight)) + matrix[x][y];
+        int minSumSoFar = Math.min(bottom, Math.min(bottomLeft, bottomRight)) + matrix[x][y];
+        cache[x][y] = minSumSoFar;
+        return minSumSoFar;
     }
 }
