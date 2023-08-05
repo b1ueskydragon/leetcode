5e7c6e5cecb19b604c247be7142174cdbae58930
ina <dragoalie@gmail.com>
2023-08-05 22:30:37 +0900

    introduce Array.prototype.at method - Array Prototype Last

--- src/main/java/leetcode/p2619/solution.js
+++ src/main/java/leetcode/p2619/solution.js
@@ -3,6 +3,11 @@ Array.prototype.last = function() {
     return (n === 0) ? -1 : this[n - 1];
 };
 
+Array.prototype.last = function() {
+    return this.length ? this.at(-1) : -1;
+};
+
+
 /**
  * const arr = [1, 2, 3];
  * arr.last(); // 3
