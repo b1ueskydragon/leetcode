a09ff022864790db968fa7b01e3ab64c9c122b3f
ina <dragoalie@gmail.com>
2025-02-23 17:35:59 +0900

    solved: Binary Tree Postorder Traversal - With simple depth-first search - Using ArrayList

---
+++ src/main/java/leetcode/p0145/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.p0145;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> postorder = new ArrayList<>();
+        dfs(postorder, root);
+        return postorder;
+    }
+
+    private void dfs(List<Integer> postorder, TreeNode root) {
+        if (root == null) {
+            return; // no more children
+        }
+        dfs(postorder, root.left);
+        // left から return して抜け出した先
+        dfs(postorder, root.right);
+        // right まで探索終わって抜け出した先
+        // stack に溜めっていた value を順次 append
+        postorder.add(root.val);
+    }
+}
