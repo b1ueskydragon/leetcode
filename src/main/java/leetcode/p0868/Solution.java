b5a52fde5e96161ce87e6479c6fb679c4de564fb
ina <dragoalie@gmail.com>
2025-02-25 18:59:55 +0900

    solved: Binary Gap - Convert integer to binary string using a StringBuilder for efficiency - Handle edge cases where the binary representation has no gap

---
+++ src/main/java/leetcode/p0868/Solution.java
@@ -0,0 +1,43 @@
+package leetcode.p0868;
+
+class Solution {
+    public int binaryGap(int n) {
+        return countDistance(convertToBinaryStr(n));
+    }
+
+    private int countDistance(String binaryStr) {
+        int start = 0;
+        final int n = binaryStr.length();
+        for (int i = 0; i < n; i++) {
+            char curr = binaryStr.charAt(i);
+            if (curr == '1') {
+                start = i;
+                break;
+            }
+        }
+
+        int maxDistance = 0;
+        int end = 0;
+        // the distance of 1001 is 3 (3-0)
+        // the distance of 11 is 1 (1-0)
+        for (int i = start; i < n; i++) {
+            char curr = binaryStr.charAt(i);
+            if (curr == '1') {
+                end = i;
+                maxDistance = Math.max(maxDistance, end - start);
+                start = end;
+            }
+        }
+        return maxDistance;
+    }
+
+    private String convertToBinaryStr(int n) {
+        StringBuilder acc = new StringBuilder();
+        while (n > 0) {
+            int bit = n % 2;
+            acc.append(bit); // 元は acc = bit + acc だけど, 遅いので StringBuilder + reverse
+            n >>= 1; // equivalent to n /= 2
+        }
+        return acc.reverse().toString();
+    }
+}
---
+++ src/test/java/leetcode/p0868/BinaryGapTest.java
@@ -0,0 +1,31 @@
+package leetcode.p0868;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BinaryGapTest {
+
+    private Solution underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new Solution();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "15, 1",
+            "9, 3",
+            "22, 2",
+            "8, 0",
+            "999999999, 3",
+            "1000000000, 3",
+            "72453911, 4"
+    })
+    void testBinaryGap(int n, int expected) {
+        assertThat(underTest.binaryGap(n)).isEqualTo(expected);
+    }
+}
