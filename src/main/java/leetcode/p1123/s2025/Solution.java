9e0eb5cacd1d73fb6925d3da3cc8cea5b07a9c8a
ina <dragoalie@gmail.com>
2025-04-10 13:26:25 +0900

    feat: Lowest Common Ancestor of Deepest Leaves (DFS + Tree DP) - Track both LCA and depth of each subtree - Return current node when both left and right depths are equal - Return deeper side’s result when one is deeper - O(N) Time complexity, O(H) Space usage   - N is the number of nodes, H is the max height(depth) of the tree

---
+++ src/main/java/leetcode/p1123/s2025/Solution.java
@@ -0,0 +1,51 @@
+package leetcode.p1123.s2025;
+
+class Solution {
+    // Note:
+    // Only one deepest leaf -> The leaf itself is a LCA
+    // Multiple deepest leaves -> Their common ancestor is a LCA
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return dfs(root).lca;
+    }
+
+    private Result dfs(TreeNode root) {
+        if (root == null) {
+            return new Result(root, 0);
+        }
+
+        Result leftRes = dfs(root.left);
+        Result rightRes = dfs(root.right);
+
+        // Already retrieved both left and right,
+        // and has returned to the node
+
+        // Left's result is deeper. Take over left's result
+        if (leftRes.level > rightRes.level) {
+            return new Result(leftRes.lca, leftRes.level + 1);
+        }
+
+        // Right's result is deeper. Take over right's result
+        if (rightRes.level > leftRes.level) {
+            return new Result(rightRes.lca, rightRes.level + 1);
+        }
+
+        // leftLca.level == rightLca.level
+        // We need to take both, and the node should be a common ancestor
+        return new Result(root, leftRes.level + 1);
+    }
+
+    // We don't care the result is from left or right
+    // Just take over the previous result
+    private static class Result {
+        // This will keep current lca
+        TreeNode lca;
+        // This indicates the depth of the deepest leaf in this subtree
+        // +1 each time as we return from children back to parent
+        int level;
+
+        Result(TreeNode lca, int level) {
+            this.lca = lca;
+            this.level = level;
+        }
+    }
+}
---
+++ src/test/java/leetcode/p1123/s2025/LcaDeepestLeavesTest.java
@@ -0,0 +1,43 @@
+package leetcode.p1123.s2025;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LcaDeepestLeavesTest {
+
+    private Solution underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testLcaDeepestLeaves(TreeNode root, TreeNode lca) {
+        assertThat(underTest.lcaDeepestLeaves(root)).isEqualTo(lca);
+    }
+
+    static Stream<Arguments> testLcaDeepestLeaves() {
+        final var root1 = new TreeNode(0);
+        root1.left = new TreeNode(1);
+        root1.right = new TreeNode(3);
+        root1.left.right = new TreeNode(2);
+
+        final var root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(3);
+
+        return Stream.of(
+                // Only one deepest leaf -> The leaf itself is a LCA
+                Arguments.of(root1, root1.left.right),
+                // Multiple deepest leaves -> Their common ancestor is a LCA
+                Arguments.of(root2, root2)
+        );
+    }
+}
