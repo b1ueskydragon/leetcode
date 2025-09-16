08b922db68346bf2e6f7ec335e3a5ffd7cb539b5
ina <dragoalie@gmail.com>
2025-09-16 20:02:00 +0900

    feat: Maximum Number of Words You Can Type (Bitmask) - Encode broken letters into a 26-bit mask (if broken, set a bit) - For each word, fail fast if any character’s bit intersects with the mask - O(|text|) Time complexity, O(1) Space usage

--- src/main/java/leetcode/p1935/Solution.java
+++ src/main/java/leetcode/p1935/Solution.java
@@ -1,31 +1,70 @@
 package leetcode.p1935;
 
 class Solution {
-    public int canBeTypedWords(String text, String brokenLetters) {
-        final boolean[] isAbsent = buildDict(brokenLetters);
-        final String[] words = text.split(" ");
-        int sum = 0;
-        for (String word : words) {
-            sum += canType(word, isAbsent);
-        }
-        return sum;
-    }
 
-    private static boolean[] buildDict(String brokenLetters) {
-        // Only consists of lowercase English letters
-        final boolean[] isAbsent = new boolean[26];
-        for (char c : brokenLetters.toCharArray()) {
-            isAbsent[c - 'a'] = true;
-        }
-        return isAbsent;
-    }
+    static class V2 {
 
-    private static int canType(String s, boolean[] isAbsent) {
-        for (char c : s.toCharArray()) {
-            if (isAbsent[c - 'a']) {
-                return 0;
+        public int canBeTypedWords(String text, String brokenLetters) {
+            int mask = 0;
+            //  0     ...    0    0    0    0
+            //  z(26) ...    d(3) c(2) b(1) a(0)
+            // if broken, set the bit to 1
+            for (char c : brokenLetters.toCharArray()) {
+                mask |= 1 << (c - 'a');
             }
+
+            int sum = 0;
+            for (String word : text.split(" ")) {
+                sum += canBeTypedWord(word, mask);
+            }
+
+            return sum;
         }
-        return 1;
+
+        private static int canBeTypedWord(String word, int mask) {
+            for (char c : word.toCharArray()) {
+                final int curr = 1 << (c - 'a');
+                if ((curr & mask) == curr) {
+                    // matched
+                    return 0;
+                }
+            }
+            return 1;
+        }
+
     }
+
+
+    static class V1 {
+
+        public int canBeTypedWords(String text, String brokenLetters) {
+            final boolean[] isAbsent = buildDict(brokenLetters);
+            final String[] words = text.split(" ");
+            int sum = 0;
+            for (String word : words) {
+                sum += canType(word, isAbsent);
+            }
+            return sum;
+        }
+
+        private static boolean[] buildDict(String brokenLetters) {
+            // Only consists of lowercase English letters
+            final boolean[] isAbsent = new boolean[26];
+            for (char c : brokenLetters.toCharArray()) {
+                isAbsent[c - 'a'] = true;
+            }
+            return isAbsent;
+        }
+
+        private static int canType(String s, boolean[] isAbsent) {
+            for (char c : s.toCharArray()) {
+                if (isAbsent[c - 'a']) {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+
+    }
+
 }
--- src/test/java/leetcode/p1935/CanBeTypedWordsTest.java
+++ src/test/java/leetcode/p1935/CanBeTypedWordsTest.java
@@ -2,27 +2,42 @@ package leetcode.p1935;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CanBeTypedWordsTest {
 
-    private Solution underTest;
+    private Solution.V2 v2;
+    private Solution.V1 v1;
 
     @BeforeEach
     void setUp() {
-        underTest = new Solution();
+        v2 = new Solution.V2();
+        v1 = new Solution.V1();
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "hello world, ad, 1",
-            "leet code, e, 0",
-            "j zihroobaqm mechvdxsapqjbueycvsakpmdsldztcxqov xgoblinn lgwheqdofkluacxtza baqxzlkp obvjwhoeooujrky, ca, 3"
-    })
-    void testCanBeTypedWords(String text, String brokenLetters, int expected) {
-        assertThat(underTest.canBeTypedWords(text, brokenLetters)).isEqualTo(expected);
+    @MethodSource("testSource")
+    void testV2(String text, String brokenLetters, int expected) {
+        assertThat(v2.canBeTypedWords(text, brokenLetters)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("testSource")
+    void testV1(String text, String brokenLetters, int expected) {
+        assertThat(v1.canBeTypedWords(text, brokenLetters)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> testSource() {
+        return Stream.of(
+                Arguments.of("hello world", "ad", 1),
+                Arguments.of("leet code", "e", 0),
+                Arguments.of("j zihroobaqm mechvdxsapqjbueycvsakpmdsldztcxqov xgoblinn lgwheqdofkluacxtza baqxzlkp obvjwhoeooujrky", "ca", 3)
+        );
     }
 
 }
