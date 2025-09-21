aa5eb3eda14a51e8a92bc1045a82cbe2b701fc80
ina <dragoalie@gmail.com>
2025-09-21 09:07:50 +0900

    feat: Add Two Numbers (Dummy node, carry) - Start with dummy head node, return head.next - Hold a carry as a variable - O(N) Time complexity and Space usage, where N = max(l1.length, l2.length)

--- src/main/java/leetcode/p0002/Solution.java
+++ src/main/java/leetcode/p0002/Solution.java
@@ -1,36 +1,71 @@
 package leetcode.p0002;
 
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        final ListNode node = new ListNode();
-        ListNode head = node;
-        while (l1 != null || l2 != null) {
-            int sum = head.val;
 
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
-            }
+    static class V2 {
 
-            if (sum >= 10) {
-                head.next = new ListNode(1); // carry
-                sum -= 10;
-            }
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            final ListNode node = new ListNode(-1); // sentinel
+            ListNode head = node;
+            int carry = 0;
+            while (l1 != null || l2 != null || carry > 0) {
+                int sum = carry;
 
-            head.val = sum;
-
-            if (l1 != null || l2 != null) {
-                if (head.next == null) {
-                    head.next = new ListNode(); // without carry
+                if (l1 != null) {
+                    sum += l1.val;
+                    l1 = l1.next;
                 }
+                if (l2 != null) {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                carry = sum / 10;
+                sum %= 10;
+                head.next = new ListNode(sum);
                 head = head.next;
             }
+
+            return node.next;
         }
 
-        return node;
     }
+
+    static class V1 {
+
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            final ListNode node = new ListNode();
+            ListNode head = node;
+            while (l1 != null || l2 != null) {
+                int sum = head.val;
+
+                if (l1 != null) {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                if (sum >= 10) {
+                    head.next = new ListNode(1); // carry
+                    sum -= 10;
+                }
+
+                head.val = sum;
+
+                if (l1 != null || l2 != null) {
+                    if (head.next == null) {
+                        head.next = new ListNode(); // without carry
+                    }
+                    head = head.next;
+                }
+            }
+
+            return node;
+        }
+
+    }
+
 }
