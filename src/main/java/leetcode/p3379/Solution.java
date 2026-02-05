5db7f9c1448d9166090d37ffcaf0715d2e779a7b
ina <dragoalie@gmail.com>
2026-02-06 00:35:52 +0900

    feat: 3379. Transformed Array - In Java, % can be negative; not a mathematical modulus operator - Add a note

--- src/main/java/leetcode/p3379/Solution.java
+++ src/main/java/leetcode/p3379/Solution.java
@@ -7,8 +7,26 @@ class Solution {
         for (int i = 0; i < n; i++) {
             int k = nums[i];
             // Normalize to non-negative. Unify all jumps to the right.
+            // e.g.,
+            // n = 4,
+            // -1 == +3
+            // -2 == +2
+            // -3 == +1
+            // -4 == +0
+            // -5 == -1 == +3
+            // ...
             if (k < 0) {
-                k = n - (Math.abs(k) % n);
+                // 数学的剰余
+                // k = n - (Math.abs(k) % n);
+
+                // Java では負数の剰余は符号付き剰余
+                // -5 % 4 = -1   (-(5 % 4))
+                // ちなみに, Python では数学的剰余
+                // -5 % 4 =  3   (4 * -2 + 3 = -5)
+                k = n + (k % n);
+
+                // ↑ 必ず k=[0, n) を保証するために二重で %n をかけても良い
+                // k = (n + (k % n)) % n;
             }
             result[i] = nums[(i + k) % n];
         }
