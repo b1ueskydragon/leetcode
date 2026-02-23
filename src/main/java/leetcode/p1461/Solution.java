859e87c7ba90f9a0fd09ff8f30f990d5e1db3c4c
ina <dragoalie@gmail.com>
2026-02-23 23:34:09 +0900

    refactor: 1461. Check If a String Contains All Binary Codes of Size K (Bit masking) - Replace subtraction to bit masking, to keep only lower k bits

--- src/main/java/leetcode/p1461/Solution.java
+++ src/main/java/leetcode/p1461/Solution.java
@@ -19,15 +19,24 @@ class Solution {
         }
         seen[window] = true;
 
+        // mask: 常に, 下位 k ビットだけ残す.
+        // e.g.,
+        // k = 2, mask = 11
+        // k = 3, mask = 111
+        final int mask = (1 << k) - 1;
+
         for (int i = k; i < n; i++) {
             final int bit = s.charAt(i) - '0';
             window = (window << 1) | bit;
+            // 上位ビットを落とす.
             // 先頭が 1 の場合, 前の残りなので先頭の 1 をとる.
             // e.g.,
-            // 110 -> 010
-            if (window >= (1 << k)) {
-                window -= (1 << k);
-            }
+            // k = 2,
+            // As-is 110 -> To-be 010,
+            // 011 & 110 = 010
+            // 先頭が 0 の場合, そのままが返ってくる.
+            // 011 & 010 = 010
+            window &= mask;
             seen[window] = true;
         }
 
