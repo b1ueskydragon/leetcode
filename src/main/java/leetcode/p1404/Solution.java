5c9bf02224a140510814b4a748e9e93219dffd05
ina <dragoalie@gmail.com>
2026-02-28 12:55:32 +0900

    refactor: 1404. Number of Steps to Reduce a Number in Binary Representation to One (Bitwise + Simulation) - Eliminate StringBuilder by tracking carry and LSB logically - Count two steps when handling odd bit (+1 and >>1) - O(N) Time complexity, O(1) Space usage

--- src/main/java/leetcode/p1404/Solution.java
+++ src/main/java/leetcode/p1404/Solution.java
@@ -2,39 +2,31 @@ package leetcode.p1404;
 
 class Solution {
     public int numSteps(String s) {
+        // The index of LSB.
+        int r = s.length() - 1;
+        int carry = 0;
         int count = 0;
-        // Reuse the stringbuilder and utilize its methods in the whole process
-        // to save the memory.
-        final var sb = new StringBuilder(s);
-        // s[0] == '1' due to the constraint, size 1 always be '1'.
-        while (sb.length() > 1) {
-            if (sb.charAt(sb.length() - 1) == '1') {
-                addOne(sb);
+        while (r > 0) {
+            // e.g.,
+            // 10011 -> 10100 -> 1010 -> 101 -> 110 -> 11 -> 100 -> 10 -> 1
+            // 1000 -> 100 -> 10 -> 1
+            // We only need to keep LSB and the current carry.
+            int lsb = s.charAt(r) - '0';
+            int carriedLsb = lsb + carry;
+            if (carriedLsb % 2 == 1) {
+                carry = (carriedLsb + 1) > 1 ? 1 : 0;
+                // +1 のあと必ず current LSB は 0 になるので, ついでに捨てる.
+                // +1, >> 1 で 2 operations.
+                count += 2;
+                r--;
             } else {
-                rightShiftOnce(sb);
+                // Right shift once, logically.
+                count++;
+                r--;
             }
-            count++;
         }
-        return count;
-    }
-
-    private void addOne(StringBuilder sb) {
-        int carry = 1;
-        for (int i = sb.length() - 1; i >= 0; i--) {
-            int res = carry + sb.charAt(i) - '0';
-            carry = (res > 1) ? 1 : 0;
-            // Replace at index.
-            sb.setCharAt(i, (char) ((res % 2) + '0'));
-
-        }
-        if (carry > 0) {
-            // Append the carry to the head.
-            sb.insert(0, carry);
-        }
-    }
-
-    private void rightShiftOnce(StringBuilder sb) {
-        // Remove the last.
-        sb.setLength(sb.length() - 1);
+        // carry: 0, lsb: 1 -> count
+        // carry: 1, lsb: 1 -> Process "10", count + 1;
+        return count + carry;
     }
 }
