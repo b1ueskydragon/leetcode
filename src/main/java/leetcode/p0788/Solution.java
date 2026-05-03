92cc89b89d569a84ec8d070962d7c8b206e4d440
ina <dragoalie@gmail.com>
2026-05-03 14:59:37 +0900

    refactor: 788. Rotated Digits - O(n log n) Time, O(1) Space

--- src/main/java/leetcode/p0788/Solution.java
+++ src/main/java/leetcode/p0788/Solution.java
@@ -5,23 +5,9 @@ class Solution {
     private static final int[] RULES = {0, 1, 5, -1, -1, 2, 9, -1, 8, 6};
 
     public int rotatedDigits(int n) {
-        final boolean[] marked = new boolean[n + 1];
-        for (int num = 1; num <= n; num++) {
-            if (marked[num]) {
-                continue;
-            }
-            final int unw = rotate(num);
-            if (unw == -1) {
-                continue;
-            }
-            marked[num] = true;
-            if (unw <= n) {
-                marked[unw] = true;
-            }
-        }
         int count = 0;
-        for (boolean t : marked) {
-            if (t) {
+        for (int num = 1; num <= n; num++) {
+            if (rotate(num) != -1) {
                 count++;
             }
         }
