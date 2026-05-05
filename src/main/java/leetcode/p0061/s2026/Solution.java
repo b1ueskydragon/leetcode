5758e71e13d185855548ba256e2981eb3ea09ccd
ina <dragoalie@gmail.com>
2026-05-05 22:55:44 +0900

    refactor: 61. Rotate List - Compute the list length and connect the tail to the head - Cut the cycle and return the new head - O(n) Time, O(1) Space

--- src/main/java/leetcode/p0061/s2026/Solution.java
+++ src/main/java/leetcode/p0061/s2026/Solution.java
@@ -7,23 +7,19 @@ class Solution {
         if (head == null || k == 0) {
             return head;
         }
-        ListNode last = head;
+        ListNode last = head; // non-null
         int n = 1;
-        while (last != null && last.next != null) {
+        while (last.next != null) {
             last = last.next;
             n++;
         }
-        ListNode newHead = head;
-        ListNode prev = null;
-        for (int i = 0; i < n - k % n; i++) {
-            prev = newHead;
-            newHead = newHead.next;
+        last.next = head; // cycle at this point
+        for (int i = 0; i < n - (k + 1) % n; i++) {
+            head = head.next;
         }
-        if (newHead == null) {
-            return head;
-        }
-        prev.next = null;
-        last.next = head;
+        // From now, the cutting point is between head and head.next.
+        ListNode newHead = head.next;
+        head.next = null; // cut the cycle
         return newHead;
     }
 }
