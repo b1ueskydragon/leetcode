b8ee6d7ddee9f02f9cae823c64d5bf7a563a897b
ina <dragoalie@gmail.com>
2026-06-06 20:17:55 +0900

    refactor: 2574. Left and Right Sum Differences - Keep only running state (left/right sums) instead of auxiliary arrays - O(n) Time, O(1) Space

--- src/main/java/leetcode/p2574/Solution.java
+++ src/main/java/leetcode/p2574/Solution.java
@@ -3,19 +3,27 @@ package leetcode.p2574;
 class Solution {
     // 1000 * 100000 < Integer.MAX_VALUE
     public int[] leftRightDifference(int[] nums) {
+        // 10,         4,        8 ,       3
+        // 10         10+4    10+4+8   10+4+8+3
+        // 3+8+4+10   3+8+4      3+8       3
+
+        // 10 -> 3+8+4 - 0
+        // 4  -> 3+8   - 10
+        // 8  -> 3     - 10+4
+        // 3  -> 0     - 10+4+8
+
         final int n = nums.length;
-        final int[] ans = new int[n];
-        for (int i = 1; i < n; i++) {
-            ans[i] = ans[i - 1] + nums[i - 1];
+        int leftAcc = 0;
+        for (int num : nums) {
+            leftAcc += num;
         }
-        int prefixSum = ans[n - 1];
-        ans[n - 1] = prefixSum;
-        int suffixSum = 0;
-        for (int i = n - 2; i >= 0; i--) {
-            prefixSum = ans[i];
-            suffixSum += nums[i + 1];
-            ans[i] = Math.abs(prefixSum - suffixSum);
+        int rightAcc = 0;
+        for (int i = 0; i < n; i++) {
+            int tmp = nums[i];
+            leftAcc -= tmp;
+            nums[i] = Math.abs(leftAcc - rightAcc);
+            rightAcc += tmp;
         }
-        return ans;
+        return nums;
     }
 }
