a79966191450ba62b45b778814d65f96c9980738
ina <dragoalie@gmail.com>
2026-06-08 22:48:20 +0900

    Override equals and hashCode for tests

--- src/main/java/leetcode/common/TreeNode.java
+++ src/main/java/leetcode/common/TreeNode.java
@@ -1,5 +1,7 @@
 package leetcode.common;
 
+import java.util.Objects;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -15,5 +17,36 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    /**
+     * For test codes.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof TreeNode otherNode)) {
+            return false;
+        }
+
+        // Objects.equals(a, b)
+        // - Null safe.
+        // - If both are non-null, calls a.equals(b).
+        // - TreeNode#equals therefore compares child nodes recursively.
+        return this.val == otherNode.val
+                && Objects.equals(this.left, otherNode.left)
+                && Objects.equals(this.right, otherNode.right);
+    }
+
+    /**
+     * For test codes.
+     */
+    @Override
+    public int hashCode() {
+        // This is convenient for tests, but may be expensive for large trees
+        // because every hashCode call traverses the whole subtree recursively.
+        return Objects.hash(val, left, right);
     }
 }
