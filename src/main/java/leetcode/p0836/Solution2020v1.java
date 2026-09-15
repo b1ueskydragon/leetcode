16bf12df9390cf1bdbf929e2dad0d695f3c8e9aa
ina <dragoalie@gmail.com>
2026-09-15 09:22:14 +0900

    rename classes

--- src/main/java/leetcode/p0836/Solution2020v1.java
+++ src/main/java/leetcode/p0836/Solution2020v1.java
@@ -1,6 +1,6 @@
 package leetcode.p0836;
 
-class Solution {
+class Solution2020v1 {
   public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
     // bottom-left corner
     final int a = rec1[0];
--- src/main/java/leetcode/p0836/Solution2020v2.java
+++ src/main/java/leetcode/p0836/Solution2020v2.java
@@ -1,6 +1,6 @@
 package leetcode.p0836;
 
-class Solution01 {
+class Solution2020v2 {
   public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
     // bottom-left corner
     final int a = rec1[0];
